abc27615a0ac6fba21d141aad5737fb2072eacd0
Rhett <chrislyons2886@gmail.com>
2021-06-16 08:32:57 -0400

    name featue aure works

--- public/js/api.js
+++ public/js/api.js
@@ -9,30 +9,37 @@
 let currentFiat;
 let companyName;
 let fiatSymbol;
-let symbolSearch;
 let searchBtn = document.getElementById('search-btn');
 let symbol = document.getElementById('symbol1')
 
-const getStock = async () => {
+const getStock = async (stockName) => {
     try{
-    const stockUrl = `https://cloud.iexapis.com/stable/stock/AAPL/quote?token={INSERT API KEY}`;
-    const stockReq = await fetch(stockUrl);
-    const stockRes = await stockReq.json();
-    currentFiat = stockRes.latestPrice;
-    companyName = stockRes.companyName;
-    fiatSymbol = stockRes.symbol;
-    return stockRes;
-    } catch {
-        console.log(error)
+        const stockUrl = `https://cloud.iexapis.com/stable/stock/${stockName}/quote?token=pk_ad61d387145f4bdeaaf6bd8749ddff4f`;
+        const stockReq = await fetch(stockUrl);
+        const stockRes = await stockReq.json();
+        currentFiat = stockRes.latestPrice;
+        companyName = stockRes.companyName;
+        fiatSymbol = stockRes.symbol;
+        var asset = {
+            comapany_name:stockRes.companyName,
+            stock_symbol:stockRes.symbol,
+            current_price:stockRes.latestPrice,
+        }
+        console.log(asset)
+        return stockRes;
+    } catch (err) {
+        console.log(err)
     }
 };
 
-getStock().then((data) => {
-    console.log(data.symbol);
-
-})
 
 searchBtn.addEventListener('click', () => {
-    symbol.innerHTML = symbolSearch.value;
-    //console.log(symbol)
-})
+    let symbolSearch = document.getElementById('symbol-search').value;
+    console.log(symbolSearch)
+
+    getStock(symbolSearch).then((data) => {
+    
+    })
+ //   symbol.innerHTML = symbolSearch.value;
+    console.log(symbol)
+}) 
